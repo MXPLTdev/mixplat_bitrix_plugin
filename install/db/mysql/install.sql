5ca649ec6c3e1d2c1decfeea406bd2e6d930dfe8
Nikolay Kravtsov <vampirus.666@gmail.com>
2020-03-20 11:22:05 +0800

    update install sql

--- install/db/mysql/install.sql
+++ install/db/mysql/install.sql
@@ -3,7 +3,7 @@ CREATE TABLE `mixplat_payment` (
   `order_id` int(11) NOT NULL,
   `payment_id` int(11) NOT NULL,
   `status` varchar(20)  NOT NULL,
-  `status_extended` varchar(20)  NOT NULL,
+  `status_extended` varchar(30)  NOT NULL,
   `date` datetime NOT NULL,
   `extra` text,
   `amount` decimal(10,2) NOT NULL,
